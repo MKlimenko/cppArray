6dc0e7db61cf676ab94bf760965fb488efd13348
Michael Klimenko <mklimenko29@gmail.com>
2024-06-30 21:56:25 +0200

    Add an option to print dimensions in the workspace plane

--- cppArray.m
+++ cppArray.m
@@ -2,12 +2,14 @@ classdef cppArray < handle
     properties (Access = private)
         % This will hold a pointer to the C++ object
         cppHandle
+        dims
     end
     
     methods
         function obj = cppArray(data)
             % Constructor: Create the C++ object and initialize with data
             obj.cppHandle = cppArrayMex('new', data);
+            obj.dims = size(data);
         end
         
         function delete(obj)
@@ -26,11 +28,26 @@ classdef cppArray < handle
             resultHandle = cppArrayMex('plus', obj.cppHandle, other.cppHandle);
             result = cppArray([]);
             result.cppHandle = resultHandle;
+            result.dims = obj.dims; % Assume the dimensions remain the same
         end
         
         function data = getData(obj)
             % Get the array data
             data = cppArrayMex('getData', obj.cppHandle);
+        end
+
+        function s = size(obj, dim)
+            % Return the size of the array
+            if nargin == 1
+                s = obj.dims;
+            else
+                s = obj.dims(dim);
+            end
+        end
+        
+        function l = length(obj)
+            % Return the length of the array
+            l = max(obj.dims);
         end
     end
 end
